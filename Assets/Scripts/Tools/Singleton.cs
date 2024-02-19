63c9cb8881b3e7824023b0fcdb7566352dd4a7ac
Barış Kılıç <barisklcc90@gmail.com>
2024-02-19 19:59:51 +0300

    Added Singleton Design Tool for simplicity

---
+++ Assets/Scripts/Tools/Singleton.cs
@@ -0,0 +1,43 @@
+using System.Runtime.CompilerServices;
+using Unity.IO.LowLevel.Unsafe;
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    private static T instance;
+
+    public static T Instance
+    { 
+        get 
+        {
+            if (instance == null)
+                Debug.Log("No instance found of " + typeof(T));
+            return instance;
+        } 
+    }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.Log("Instance found" + typeof(T) + "Self Destructing");
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this as T;
+            Init();
+        }
+    }
+
+    protected void OnDestroy()
+    {
+        if (this == instance)
+            instance = null;
+    }
+
+    protected virtual void Init()
+    {
+
+    }
+}
